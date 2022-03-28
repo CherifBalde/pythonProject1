282bb8f2daf40f1cb5041c808da1c15a042fd1ac
CHERIF BALDE <cherifbalde@CHERIFs-iMac.local>
2022-03-27 21:27:27 -0700

    Initial commit

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Mamadou Balde
+# GitHub username:
+# Date: 3/26/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
